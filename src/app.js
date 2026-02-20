a93c4baf5d38472436b1c3119ad7656a91f961a9
AyushAgrawal18 <agrawalayush638@gmail.com>
2026-02-20 23:04:46 +0530

    Connect MySQL and Create DataBase hospital

--- src/app.js
+++ src/app.js
@@ -1,10 +1,20 @@
 const express = require('express');
 const app = express();
+const db = require('./config/db');
 
 app.use(express.json());
 
 app.get('/', (req, res) => {
     res.send("Hospital API Running");
+});
+
+app.get('/test-db', async (req, res) => {
+    try {
+        const [rows] = await db.query("SELECT 1");
+        res.json({ message: "Database Connected", rows });
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
 });
 
 module.exports = app;
